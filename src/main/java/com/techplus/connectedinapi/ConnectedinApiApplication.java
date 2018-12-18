cf650e1433895e9f29f77bf563e239624c7c30c5
DanielDPN <danielpereiradonascimento@gmail.com>
2018-12-18 16:28:25 -0300

    first commit

---
+++ src/main/java/com/techplus/connectedinapi/ConnectedinApiApplication.java
@@ -0,0 +1,14 @@
+package com.techplus.connectedinapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectedinApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConnectedinApiApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/techplus/connectedinapi/ConnectedinApiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.techplus.connectedinapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConnectedinApiApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
