16c39f9f98e25fdfb473d8661beffd14fcb4034d
DanielDPN <danielpereiradonascimento@gmail.com>
2019-01-05 17:54:52 -0300

    Add Trigger After Insert Post

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -1,5 +1,5 @@
 -- Trigger after_update on invitations
-CREATE TRIGGER after_update
+CREATE TRIGGER after_update_invitations
   AFTER UPDATE
   ON invitations FOR EACH ROW
 BEGIN
@@ -7,4 +7,12 @@ BEGIN
     INSERT INTO users_contacts (user_id, contact_id) VALUES (NEW.sender_id, NEW.receiver_id);
     INSERT INTO users_contacts (user_id, contact_id) VALUES (NEW.receiver_id, NEW.sender_id);
   END IF;
+END;
+
+-- Trigger after_insert on posts
+CREATE TRIGGER after_insert_posts
+  AFTER INSERT
+  ON posts FOR EACH ROW
+BEGIN
+  INSERT INTO users_posts (user_id, post_id) VALUES (NEW.owner_id, NEW.id);
 END;
